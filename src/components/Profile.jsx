aeb7b920ce72cf7ff728e24c9a38b5e97142ef36
Ironfist <ironfistse@gmail.com>
2021-06-27 19:29:10 +0300

    Divide App into components: Header, Navbar, Profile

---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+const Profile = () => {
+  return (
+    <div className="App-content">
+      <div className="banner">
+        <img
+          src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350"
+          alt="Nature"
+        />
+      </div>
+      <div>ava + descrption</div>
+      <div>
+        my posts
+        <div>New post</div>
+      </div>
+      <div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
