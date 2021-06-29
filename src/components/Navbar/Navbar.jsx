2bad22ee76b35eb681377d84e777c334cdfdd7de
Ironfist <ironfistse@gmail.com>
2021-06-29 17:44:03 +0300

    Add clickable links to navigation.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,24 @@
+import { NavLink } from 'react-router-dom';
 import classes from './Navbar.module.css';
 
 const Navbar = () => {
   return (
     <nav className={classes.nav}>
-      <div className={`${classes.item} ${classes.active}`}>Profile</div>
-      <div className={classes.item}>Messages</div>
-      <div className={classes.item}>News</div>
-      <div className={classes.item}>Music</div>
-      <div className={classes.item}>Settings</div>
+      <div className={classes.item}>
+        <NavLink to='/profile' activeClassName={classes.active}>Profile</NavLink>
+      </div>
+      <div className={classes.item}>
+        <NavLink to='/messages' activeClassName={classes.active}>Messages</NavLink>
+      </div>
+      <div className={classes.item}>
+        <NavLink to='/news' activeClassName={classes.active}>News</NavLink>
+      </div>
+      <div className={classes.item}>
+        <NavLink to='/music' activeClassName={classes.active}>Music</NavLink>
+      </div>
+      <div className={classes.item}>
+        <NavLink to='/settings' activeClassName={classes.active}>Settings</NavLink>
+      </div>
     </nav>
   );
 }
