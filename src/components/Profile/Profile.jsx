8585bf42da6f7bf47f130bf259d53d7aa00a8c1b
Ironfist <ironfistse@gmail.com>
2021-06-29 13:21:09 +0300

    Create folder for each component.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import classes from './Profile.module.css';
+import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
   return (
@@ -10,14 +11,7 @@ const Profile = () => {
         />
       </div>
       <div>ava + descrption</div>
-      <div>
-        my posts
-        <div>New post</div>
-      </div>
-      <div>
-        <div className={classes.item}>post 1</div>
-        <div className={classes.item}>post 2</div>
-      </div>
+      <MyPosts />
     </div>
   );
 }
