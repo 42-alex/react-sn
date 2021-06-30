aca1667debc6379e652fddd2e73d47328faf6e79
Ironfist <ironfistse@gmail.com>
2021-06-30 18:56:19 +0300

    Output Dialogs content with map property.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Message = (props) => {
   return (
-    <div>{props.text}</div>
+    <div>{props.message}</div>
   )
 }
 
@@ -14,17 +14,25 @@ const DialogItem = (props) => {
 }
 
 const Dialogs = () => {
+  const dialogs = [
+    { id: 1, name: 'Oliver' },
+    { id: 2, name: 'Neil' },
+    { id: 3, name: 'Stewart' },
+    { id: 4, name: 'Sonya' },
+  ];
+  const messages = [
+    { id: 1, message: 'Message 1' },
+    { id: 2, message: 'How are you?' },
+    { id: 3, message: 'What is the capital of Great Britain?' },
+  ];
+
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialogsItems}>
-        <DialogItem id="1" name="Oliver" />
-        <DialogItem id="2" name="Neil" />
-        <DialogItem id="3" name="Stewart" />
+        { dialogs.map(el => <DialogItem key={el.id} id={el.id} name={el.name} />) }
       </div>
       <div className={classes.messages}>
-        <Message text="Message 1" />
-        <Message text="How are you?" />
-        <Message text="What is the capital of Great Britain?" />
+        { messages.map(el => <Message key={el.id} message={el.message} />) }
       </div>
     </div>
   );
