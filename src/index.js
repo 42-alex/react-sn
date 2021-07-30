f11560389afb8b102a46361895b21f3bc7db5827
Ironfist <ironfistse@gmail.com>
2021-07-30 17:10:30 +0300

    Remove rerender function (react-redux do subscribe under the hood)

--- src/index.js
+++ src/index.js
@@ -5,17 +5,11 @@ import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
 
-const renderEntireApp = () => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store} >
-        <App />
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
-
-renderEntireApp();
-
-store.subscribe(renderEntireApp);
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App/>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
