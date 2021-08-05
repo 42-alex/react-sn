806e652759a550c25e4ff9514e4b572129f92059
Ironfist <ironfistse@gmail.com>
2021-08-05 22:07:33 +0300

    Show preloader if there is no userProfile fetched yet.

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,11 @@
+import Preloader from '../../common/Preloader';
 import classes from './ProfileInfo.module.css';
 
 const ProfileInfo = (props) => {
+  if (!props.userProfile) {
+    return <Preloader />
+  }
+
   return (
     <>
       <div className={classes.banner}>
@@ -9,15 +14,13 @@ const ProfileInfo = (props) => {
           alt="Nature"
         />
       </div>
-      {props.userProfile &&
-        <div className={classes.profileDescription}>
-          <div>Full name: {props.userProfile.fullName}</div>
-          <div>About me: {props.userProfile.aboutMe}</div>
-          <div>
-            <img src={props.userProfile.photos.small} alt="Profile" />
-          </div>
+      <div className={classes.profileDescription}>
+        <div>Full name: {props.userProfile.fullName}</div>
+        <div>About me: {props.userProfile.aboutMe}</div>
+        <div>
+          <img src={props.userProfile.photos.small} alt="Profile" />
         </div>
-      }
+      </div>
     </>
   )
 };
