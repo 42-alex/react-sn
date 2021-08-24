956b07bb6dc824960da2e55d17c48b3aa7c9fc8d
Ironfist <ironfistse@gmail.com>
2021-08-24 20:09:22 +0300

    Refactoring: move markup for each User on Users page to separated component.

---
+++ src/components/Users/User.js
@@ -0,0 +1,41 @@
+import { NavLink } from 'react-router-dom';
+import defaultAvatar from '../../assets/images/avatar.jpg';
+import { DEFAULT_USER_CITY, DEFAULT_USER_CONTRY, DEFAULT_USER_STATUS } from '../../const/settings';
+import classes from './User.module.css';
+
+
+const User = ({user, followingInProgress, followUser, unfollowUser}) => {
+  return (
+    <div key={user.id} className={classes.userItem}>
+      <div className={classes.userAvatar}>
+        <div className={classes.avatarWrapper}>
+          <NavLink to={`/profile/${user.id}`}>
+            <img src={user.photos.small || defaultAvatar} alt="user avatar" className={classes.avatarImg}/>
+          </NavLink>
+        </div>
+        {user.followed
+          ? <button
+            disabled={followingInProgress.includes(user.id)}
+            onClick={() => unfollowUser(user.id)}
+          >Unfollow</button>
+          : <button
+            disabled={followingInProgress.includes(user.id)}
+            onClick={() => followUser(user.id)}
+          >Follow</button>
+        }
+      </div>
+      <div className={classes.userInfo}>
+        <div className={classes.mainUserInfo}>
+          <div className={classes.userName}>{user.name}</div>
+          <div className={classes.userStatus}>{user.status || DEFAULT_USER_STATUS}</div>
+        </div>
+        <div className={classes.locationInfo}>
+          <div className={classes.userCountry}>{DEFAULT_USER_CONTRY}</div>
+          <div className={classes.userCity}>{DEFAULT_USER_CITY}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default User;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,49 +1,14 @@
 import React from 'react';
 import classes from './Users.module.css';
-import defaultAvatar from '../../assets/images/avatar.jpg';
-import {
-  DEFAULT_USER_STATUS,
-  DEFAULT_USER_CONTRY,
-  DEFAULT_USER_CITY,
-} from '../../const/settings';
 import Preloader from '../common/Preloader';
-import { NavLink } from 'react-router-dom';
 import Paginator from '../common/Paginator/Paginator';
+import User from './User';
 
 const Users = (props) => {
   const { currentPage, usersOnPage, totalUsersCount, onPageNumberClick } = props;
   const usersItems = props.users.map(
-    (user) => (
-      <div key={user.id} className={classes.userItem}>
-        <div className={classes.userAvatar}>
-          <div className={classes.avatarWrapper}>
-            <NavLink to={`/profile/${user.id}`}>
-              <img src={user.photos.small || defaultAvatar} alt="user avatar" className={classes.avatarImg} />
-            </NavLink>
-          </div>
-          {user.followed
-            ? <button
-              disabled={props.followingInProgress.includes(user.id)}
-              onClick={() => props.unfollowUser(user.id)}
-            >Unfollow</button>
-            : <button
-              disabled={props.followingInProgress.includes(user.id)}
-              onClick={() => props.followUser(user.id)}
-            >Follow</button>
-          }
-        </div>
-        <div className={classes.userInfo}>
-          <div className={ classes.mainInfo }>
-            <div className={ classes.userName }>{ user.name }</div>
-            <div className={ classes.userStatus }>{ user.status || DEFAULT_USER_STATUS }</div>
-          </div>
-          <div className={ classes.locationInfo }>
-            <div className={ classes.userCountry }>{ DEFAULT_USER_CONTRY }</div>
-            <div className={ classes.userCity }>{ DEFAULT_USER_CITY }</div>
-          </div>
-        </div>
-      </div>
-    )
+    (user) => <User key={user.id} user={user} followingInProgress={props.followingInProgress}
+                    followUser={props.followUser} unfollowUser={props.unfollowUser} />
   );
 
   return <>
