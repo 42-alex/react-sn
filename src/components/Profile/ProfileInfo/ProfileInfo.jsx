8604dc3b2a19e5e0d7a8a7455285d6cfbe23d0a5
Ironfist <ironfistse@gmail.com>
2021-08-26 13:44:25 +0300

    Fix ProfileInfo markup.

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -10,17 +10,11 @@ const ProfileInfo = (props) => {
 
   return (
     <>
-      <div className={classes.banner}>
-        <img
-          src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350"
-          alt="Nature"
-        />
-      </div>
       <div className={classes.profileDescription}>
         <div>Full name: {props.userProfile.fullName}</div>
         <div>About me: {props.userProfile.aboutMe}</div>
         <div>
-          <img src={props.userProfile.photos.small || defaultAvatar} alt="Profile" />
+          <img src={props.userProfile.photos.small || defaultAvatar} alt="Profile" className={classes.mainPhoto} />
         </div>
         <ProfileStatus status={ props.userProfile.status } setUserStatus={ props.setUserStatus } />
       </div>
