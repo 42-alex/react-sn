b5286ee51a06dd83a6edfd0273fbf9677ddeac7f
Ironfist <ironfistse@gmail.com>
2021-08-27 22:48:55 +0300

    Animate error appearance / disappearance

--- src/components/GlobalErrorPopup/GlobalErrorPopup.jsx
+++ src/components/GlobalErrorPopup/GlobalErrorPopup.jsx
@@ -1,20 +1,35 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import c from './GlobalErrorPopup.module.css';
 
 
 const GlobalErrorPopup = ({globalErrorMessage, setGlobalError}) => {
+  let errorWrapperDiv = useRef(null);
+  let errorDiv= useRef(null)
+
   useEffect(() => {
     if (globalErrorMessage) {
+      // clear error after 4 sec
       setTimeout(() => {
         setGlobalError('');
       }, 4000);
+
+      // animate error appearance and disappearance
+      setTimeout(() => {
+        errorWrapperDiv.current.style.height = '60px';
+        errorDiv.current.style.height = '60px';
+      }, 100);
+
+      setTimeout(() => {
+        errorWrapperDiv.current.style.height = '0';
+        errorDiv.current.style.height = '0';
+      }, 3000);
     }
   }, [globalErrorMessage])
 
   return (
     globalErrorMessage
-    ? <div className={c.errorWrapper}>
-        <div>{ globalErrorMessage }</div>
+    ? <div ref={errorWrapperDiv} className={c.errorWrapper}>
+        <div ref={errorDiv}>{ globalErrorMessage }</div>
       </div>
     : null
   )
