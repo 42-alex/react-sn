3ba71d9e30aa513a7bf09dbc84b568f076b1065e
Ironfist <ironfistse@gmail.com>
2021-08-31 12:07:32 +0300

    Add typization for app reducer.

--- src/redux/reducers/app-reducer.ts
+++ src/redux/reducers/app-reducer.ts
@@ -4,15 +4,25 @@ import { authMe } from './auth-reducer';
 const SET_APP_INITIALIZATION = 'social_network/app/SET_APP_INITIALIZATION';
 const SET_GLOBAL_ERROR = 'social_network/app/SET_GLOBAL_ERROR';
 
-const initialState = {
+type ActionType = {
+  type: typeof SET_APP_INITIALIZATION | typeof SET_GLOBAL_ERROR
+  globalErrorMessage?: string
+}
+
+export type StateType = {
+  initialized: boolean
+  globalErrorMessage: string | undefined
+};
+
+const initialState: StateType = {
   initialized: false,
   globalErrorMessage: '',
 }
 
-const appReducer = (state = initialState, action) => {
+const appReducer = (state = initialState, action: ActionType): StateType => {
   switch(action.type) {
     case SET_APP_INITIALIZATION:
-      return { ...state, initialized: true }
+      return { ...state, initialized: true, }
     case SET_GLOBAL_ERROR:
       return { ...state, globalErrorMessage: action.globalErrorMessage }
     default:
@@ -20,16 +30,16 @@ const appReducer = (state = initialState, action) => {
   }
 }
 
-export const setAppInitialization = () => ({
+export const setAppInitialization = (): ActionType => ({
   type: SET_APP_INITIALIZATION,
 })
 
-export const setGlobalError = (globalErrorMessage) => ({
+export const setGlobalError = (globalErrorMessage: string): ActionType => ({
   type: SET_GLOBAL_ERROR,
   globalErrorMessage
 })
 
-export const initializeApp = () => (dispatch) => {
+export const initializeApp = () => (dispatch: any) => {
   const authMePromise = dispatch(authMe());
   Promise.all([authMePromise])
     .then(() => {
